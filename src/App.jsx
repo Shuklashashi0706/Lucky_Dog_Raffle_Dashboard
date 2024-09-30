029c2151f814ee9e4bee64793781befcd0a97a00
prithvikr66 <pkunofficial66@gmail.com>
2024-10-01 00:40:10 +0530

    [initial commit]

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import GlobalMetrics from "./components/GlobalMetrics";
+
+function App() {
+  return (
+    <div className="">
+      <GlobalMetrics />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GlobalMetrics.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GlobalMetrics = () => {
+  return (
+    <div>
+      <h2 className=" text-[30px] text-black">Global Metrics</h2>
+      <div>
+        <p>Total Raffles Created <span></span></p>
+      </div>
+    </div>
+  );
+};
+
+export default GlobalMetrics;
