2e230d82c13b8dc83231b3a91a00e6a57d4c6aad
prithvikr66 <pkunofficial66@gmail.com>
2024-10-10 01:21:41 +0530

    [added raffle pool for active raffles]

--- src/components/RafflePool.jsx
+++ src/components/RafflePool.jsx
@@ -1,46 +1,70 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
+
 const RafflePool = () => {
   const [raffles, setRaffles] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [isActive, setIsActive] = useState(false);
   useEffect(() => {
     const fetchMetrics = async () => {
+      setLoading(true);
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_BASE_URI}/api/v1/raffle-pool`
+          `${
+            import.meta.env.VITE_BASE_URI
+          }/api/v1/raffle-pool?isActive=${isActive}`
         );
         setRaffles(response.data);
-        console.log(raffles);
+        console.log(response.data);
         setLoading(false);
       } catch (err) {
+        console.log(err);
         setError("Failed to fetch metrics");
         setLoading(false);
       }
     };
 
     fetchMetrics();
-  }, []);
+  }, [isActive]);
+
+  const handleDropdownChange = (e) => {
+    setIsActive(e.target.value);
+  };
+
   return (
-    <div className=" border border-black p-[20px] rounded-[10px]">
-      <h2 className=" text-[30px] text-black font-bold text-center">
-        Active Raffles
-      </h2>
-      <div className="flex justify-between font-bold text-[20px] mt-[10px] mb-[10px]">
-        <p>Raffle Id</p>
-        <p>Raffle Title</p>
-        <p>Raffle Pool</p>
+    <div className="border border-black p-[20px] rounded-[10px]">
+      <div className=" flex  justify-between pr-[50px] pl-[0px] mb-[10px]">
+        <h2 className="text-[30px] text-black font-bold text-center">
+          Active Raffles
+        </h2>
+        <select value={isActive} onChange={handleDropdownChange} className=" p-[5px] rounded-[10px] ">
+          <option value="true">Active Raffles</option>
+          <option value="false">Completed Raffles</option>
+        </select>
       </div>
-      <div className=" font-medium">
+      <div className="flex justify-between font-bold text-[20px] mt-[10px] mb-[10px]">
+        <p className="w-[20%]">Raffle Id</p>
+        <p className="w-[20%]">Raffle Title</p>
+        <p className="w-[20%]">Created By</p>
+        <p className="w-[20%]">Group</p>
+        <p className="w-[20%]">Raffle Pool</p>
+      </div>
+
+      <div className="font-medium">
         {loading ? (
-          <p>loading</p>
+          <p>Loading...</p>
+        ) : error ? (
+          <p>{error}</p>
         ) : (
-          raffles.map((raffle) => (
-            <div className=" flex justify-between  ">
-              <p>{raffle.raffleId}</p>
-              <p>{raffle.raffleTitle}</p>
-              <p>{raffle.rafflePool} ETH</p>
+          raffles?.map((raffle) => (
+            <div key={raffle.raffleId} className="flex justify-between">
+              <p className="w-[20%]">{raffle.raffleId}</p>
+              <p className="w-[20%]">{raffle.raffleTitle}</p>
+              <p className="w-[20%]">{raffle.userId}</p>
+              <p className="w-[20%]">{raffle.groupName}</p>
+              <p className="w-[20%]">{raffle.rafflePool} ETH</p>
             </div>
           ))
         )}
